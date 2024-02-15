1745e71ef6781500e075ce2f1769a27fe7e05cf8
Нурсултан Аалыев <nusi.aalyev@gmail.com>
2024-02-15 15:35:55 +0600

    update

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "example" {
   name = "demoxxaa-bucket"
   location = "US"
   force_destroy = true
-
+  project = "flutterapp-9075d"
   public_access_prevention = "enforced"
 }
